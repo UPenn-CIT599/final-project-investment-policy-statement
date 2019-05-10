719d92703ae415d0dbee4d745edae42d3cc2eff1
Mengxue Li <mengxueli2015@gmail.com>
2019-05-09 21:58:47 -0400

    change to use junit test lib

--- src/edu/upenn/seas/mcit591/ips/view/FutureValueCalcTest.java
+++ src/edu/upenn/seas/mcit591/ips/view/FutureValueCalcTest.java
@@ -1,8 +1,6 @@
 package edu.upenn.seas.mcit591.ips.view;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 class FutureValueCalcTest {
 
